6fd20fa481ef5bbc2d40eccb29d451d112ef4fc6
Dirk Morris <dmorris@untangle.com>
2013-07-16 16:11:31 +0000

    remove virtualbox
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/upstream/pkgs@35297 8ebf8b4e-06f3-0310-961f-89f74618be54

--- src/kernel-modules/virtualbox-ose-guest/vboxadd/version-generated.h
+++
@@ -1,9 +0,0 @@
-#ifndef ___version_generated_h___
-#define ___version_generated_h___
-
-#define VBOX_VERSION_MAJOR 1
-#define VBOX_VERSION_MINOR 6
-#define VBOX_VERSION_BUILD 6
-#define VBOX_VERSION_STRING "1.6.6_OSE"
-
-#endif
--- src/kernel-modules/virtualbox-ose-guest/vboxguest/version-generated.h
+++
@@ -1,9 +0,0 @@
-#ifndef ___version_generated_h___
-#define ___version_generated_h___
-
-#define VBOX_VERSION_MAJOR 3
-#define VBOX_VERSION_MINOR 2
-#define VBOX_VERSION_BUILD 4
-#define VBOX_VERSION_STRING "3.2.4_OSE"
-
-#endif
